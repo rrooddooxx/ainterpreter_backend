5063e2e66814c67fe4d9bdd6c1bf0888deafb4ff
pony-v <juan.valdes@applydigital.com>
2024-08-22 14:53:50 -0400

    add venv to gitignore

--- whisper-live/app.py
+++ whisper-live/app.py
@@ -5,7 +5,7 @@ client = TranscriptionClient(
   9090,
   lang="es",
   translate=False,
-  model="tiny",
+  model="small",
   use_vad=True,
   save_output_recording=False,                         # Only used for microphone input, False by Default
   output_recording_filename="./output_recording.wav"  # Only used for microphone input
