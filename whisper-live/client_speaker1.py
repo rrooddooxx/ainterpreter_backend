9212cad193005ee74c9e78be1f305a2f0b4795dd
Sebastián Kravetz <sebastiankravetz@icloud.com>
2024-08-24 11:50:03 -0400

    .

---
+++ whisper-live/client_speaker1.py
@@ -0,0 +1,16 @@
+from whisper_live.client import TranscriptionClient
+
+print("CLIENTE #1")
+
+client = TranscriptionClient(
+  "localhost",
+  9090,
+  lang="es",
+  translate=False,
+  model="tiny",
+  use_vad=True,
+  save_output_recording=False,                         # Only used for microphone input, False by Default
+  output_recording_filename="./output_recording.wav"  # Only used for microphone input
+)
+
+client(rtsp_url="rtsp://localhost:8554/test")
--- whisper-live/venv/bin/activate.fish
+++ whisper-live/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sebastiankravetz/Dev/Projects/AInterpreter_Hackathon/backend/WhisperLiveServer/venv"
+set -gx VIRTUAL_ENV "/Users/sebastiankravetz/Dev/Projects/AInterpreter_Hackathon/backend/whisper-live/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
